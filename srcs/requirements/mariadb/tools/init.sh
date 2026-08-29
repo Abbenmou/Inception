af9b5b20c84ef01c08d3bf2435cdff84ade84cfe
Abbenmou <abderrahmanebenmoussa79@gmail.com>
2026-08-29 20:05:00 +0100

    modif

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,20 +6,21 @@ DB_USER=$(cat /run/secrets/db_user.txt)
 DB_PASS=$(cat /run/secrets/db_password.txt)
 DB_ROOT_PASS=$(cat /run/secrets/db_root_password.txt)
 
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    mysqld_safe --skip-networking & 
-    until mysqladmin ping --silent; do sleep 1; done
-
-    mysql -u root <<EOF
+    cat > /tmp/init.sql <<EOF
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
 FLUSH PRIVILEGES;
 EOF
-    mysqladmin -u root -p"${DB_ROOT_PASS}" shutdown
+
+    exec mariadbd --user=mysql --datadir=/var/lib/mysql --init-file=/tmp/init.sql
 fi
 
-exec "$@"
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+DB_NAME=$(cat /run/secrets/db_name.txt)
+DB_USER=$(cat /run/secrets/db_user.txt)
+DB_PASS=$(cat /run/secrets/db_password.txt)
+DB_HOST="${DB_HOST:-mariadb}"
+
+WP_ADMIN_USER=$(cat /run/secrets/wp_admin_user.txt)
+WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_password.txt)
+WP_ADMIN_EMAIL=$(cat /run/secrets/wp_admin_email.txt)
+WP_TITLE="${WP_TITLE:-Inception}"
+WP_URL="${DOMAIN_NAME:-localhost}"
+
+WP_DIR=/var/www/html
+
+mkdir -p "$WP_DIR"
+chown -R www-data:www-data "$WP_DIR"
+
+if [ ! -f "$WP_DIR/wp-config.php" ]; then
+    su -s /bin/bash www-data -c "
+        wp core download --path='$WP_DIR' --locale=en_US \
+        && wp config create \
+                --path='$WP_DIR' \
+                --dbname='$DB_NAME' \
+                --dbuser='$DB_USER' \
+                --dbpass='$DB_PASS' \
+                --dbhost='$DB_HOST' \
+        && wp core install \
+                --path='$WP_DIR' \
+                --url='$WP_URL' \
+                --title='$WP_TITLE' \
+                --admin_user='$WP_ADMIN_USER' \
+                --admin_password='$WP_ADMIN_PASS' \
+                --admin_email='$WP_ADMIN_EMAIL' \
+                --skip-email
+    "
+    chown -R www-data:www-data "$WP_DIR"
+fi
+
+exec php-fpm8.2 -F
